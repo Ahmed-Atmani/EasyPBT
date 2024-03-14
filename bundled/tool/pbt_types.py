1c9f30883780991358b5382c63abed11444bf9b5
Ahmed Atmani <ahmed.khalil-allah.atmani@vub.be>
2024-03-14 12:18:05 +0100

    Added real PBT type descriptions

--- bundled/tool/pbt_types.py
+++ bundled/tool/pbt_types.py
@@ -18,77 +18,77 @@ pbtTypes = [
     {
         "typeId": PbtTypeId.DIFF_PATH_SAME_DEST,
         "name": 'Different Paths, Same Destination',
-        "description": 'Different Paths, Same Destination__"description"',
+        "description": 'Tests the associativity of a function [e.g. add(2, 3) == add(3, 2)]',
         "argument": '--equivalent',
         "twoFunctions": False
     },
     {
         "typeId": PbtTypeId.ROUNDTRIP,
         "name": 'Roundtrip',
-        "description": 'Roundtrip__"description"',
+        "description": 'Tests a function and its inverse [e.g. decode(encode("msg")) == "msg"]',
         "argument": '--roundtrip',
         "twoFunctions": True
     },
     {
         "typeId": PbtTypeId.SOME_THINGS_NEVER_CHANGE,
         "name": 'Some Things Never Change',
-        "description": 'Some Things Never Change__"description"',
+        "description": 'Tests the idempotence of a function [e.g. abs(5) == 5]',
         "argument": '--idempotent',
         "twoFunctions": False
     },
     {
         "typeId": PbtTypeId.THE_MORE_THINGS_CHANGE,
         "name": 'The More Things Change, the More They Stay the Same',
-        "description": 'Some Things Never Change__"description"',
+        "description": 'Tests the idempotence of a function after one call [e.g. sorted(sorted(list))) == sorted(list)]',
         "argument": '--idempotent',
         "twoFunctions": False
     },
     {
         "typeId": PbtTypeId.SOLVE_SMALLER_PROBLEM_FIRST,
         "name": 'Solve a Smaller Problem First',
-        "description": 'Solve a Smaller Problem First__"description"',
+        "description": 'Tests the smaller parts of a data structure [e.g. test(list[0]); test(list[1]); ...]',
         "argument": '',
         "twoFunctions": False
     },
     {
         "typeId": PbtTypeId.HARD_TO_PROVE,
         "name": 'Hard to Prove, Easy to Verify',
-        "description": 'Hard to Prove, Easy to Verify__"description"',
+        "description": 'Tests the output of a function using a predefined predicate [e.g. isCorrect(solution)]',
         "argument": '--equivalent',
         "twoFunctions": False
     },
     {
         "typeId": PbtTypeId.TEST_ORACLE,
         "name": 'The Test oracle',
-        "description": 'Test oracle__"description"',
+        "description": 'Compares a function with a reference function [e.g. potentiallyCorrectSort(list) == quicksort(list)]',
         "argument": '--equivalent',
         "twoFunctions": True
     },
     {
         "typeId": PbtTypeId.MODEL_BASED,
         "name": 'Model Based',
-        "description": 'Model Based__"description"',
+        "description": 'Variant of test oracle, but a simpler version of the function is the reference [e.g. newFunc(x) == simplifiedFunc(x)]',
         "argument": '',
         "twoFunctions": True
     },
     {
         "typeId": PbtTypeId.WITHIN_EXPECTED_BOUNDS,
         "name": 'Outputs Within Expected Bounds',
-        "description": 'Outputs Within Expected Bounds__"description"',
+        "description": 'Tests the output domain of a function [e.g. sin(x) >= -1 and sin(x) <= 1]',
         "argument": '',
         "twoFunctions": False
     },
     {
         "typeId": PbtTypeId.METAMORPHIC_PROP,
         "name": 'Metamorphic Property',
-        "description": 'Metamorphic Property__"description"',
+        "description": 'Variant of test oracle, but the outputs are compared [e.g. newCompiler(src)(5) == oldCompiler(src)(5))]',
         "argument": '--equivalent',
         "twoFunctions": True
     },
     {
         "typeId": PbtTypeId.UNKNOWN,
         "name": 'Unknown',
-        "description": 'Let the type be automatically determined',
+        "description": 'Let the type be automatically determined by Hypothesis\' Ghostwriter function',
         "argument": '',
         "twoFunctions": False
     }
